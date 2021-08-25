3e2e3a79cfbce4f58cf9903001a2bf752a75e9c8
Nirbhay Gupta <guptanirbhay93@gmail.com>
2021-08-25 18:24:36 +0530

    [Package] Fix

--- setup.py
+++ setup.py
@@ -14,4 +14,23 @@ setup(
   license="MIT license",
   include_package_data=True,
   zip_safe=False,
+  install_requires=[
+    "attrs==21.2.0",
+    "certifi==2021.5.30",
+    "charset-normalizer==2.0.4",
+    "colorama==0.4.4",
+    "idna==3.2",
+    "iniconfig==1.1.1",
+    "marshmallow==3.13.0",
+    "packaging==21.0",
+    "pluggy==0.13.1",
+    "py==1.10.0",
+    "pyaml==21.8.3",
+    "pyparsing==2.4.7",
+    "pytest==6.2.4",
+    "PyYAML==5.4.1",
+    "requests==2.26.0",
+    "toml==0.10.2",
+    "urllib3==1.26.6"
+  ]
 )
