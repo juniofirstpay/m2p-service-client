d341350ac895dc06c12f03a434835e237af5fc3b
Nirbhay Gupta <guptanirbhay93@gmail.com>
2022-12-11 01:36:49 +0530

    [config] environment bug fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeta-micro-client',
     packages=['zeta_micro_client'],
-    version='0.8.7',
+    version='0.8.9',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
