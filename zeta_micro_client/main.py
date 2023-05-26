7a45d22e1b1741d778805f930c0b00a3993e5942
Anikait <anikait.makkar@junio.in>
2023-05-26 16:43:52 +0530

    [feat][kyc-token] expose method on main client

--- zeta_micro_client/main.py
+++ zeta_micro_client/main.py
@@ -262,6 +262,10 @@ class ZetaMicroClient(object):
             account_holder_id=account_holder_id
         )
         return response
+    
+    def get_account_holder_kyc_token(self, account_holder_id: str):
+        response = self.zeta_service.get_account_holder_kyc_token(account_holder_id=account_holder_id)
+        return response
 
     def get_resource_txns(self, resource_id: str):
         response = self.zeta_service.fetch_resource_transactions(
