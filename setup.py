be13752bccc8b8adf25c312a7fece2687afd8820
Nirbhay Gupta <nirbhay.gupta@junio.in>
2024-01-23 15:01:45 +0530

    [release] v1.0.7 update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeta-micro-client',
     packages=['zeta_micro_client'],
-    version='1.0.2',
+    version='1.0.7',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
